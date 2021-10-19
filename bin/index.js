98eb83442e5294eb52ffb8320a57047393804ac9
Sergey Batukov <sashachepuha@gmail.com>
2021-10-19 10:17:14 +0000

    add menu

--- bin/index.js
+++ bin/index.js
@@ -1,42 +1,23 @@
 #!/usr/bin/env node
 
 const program = require('commander');
-
-// import function to list coffeee menu
 const list = require('../lib/list');
+const task = require('../lib/task');
 
-// import function to order a coffee
-const order = require('../lib/order');
-
-/*******************************************/
-
-// Print coffee drinks menu
-// $ coffee-shop list
-// $ coffee-shop ls
 program
-    .command('list') // sub-command name
-    .alias('ls') // alternative sub-command is `al`
-    .description('List coffee menu') // command description
-
-    // function to execute when command is uses
+    .command('list')
+    .alias('ls')
+    .description('Список проблем')
     .action(function () {
         list();
     });
 
-
-// Order a coffee
-// $ coffee-shop order
-// $ coffee-shop o
 program
-    .command('order') // sub-command name
-    .alias('o') // alternative sub-command is `o`
-    .description('Order a coffee') // command description
-
-    // function to execute when command is uses
+    .command('task') 
+    .alias('t')
+    .description('Выбор проблемы')
     .action(function () {
-        order();
+        task();
     });
 
-
-// allow commander to parse `process.argv`
 program.parse(process.argv);
